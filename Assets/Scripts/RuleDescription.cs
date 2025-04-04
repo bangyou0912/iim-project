50303c5587c6120b51a8bf80bf0fc75076343d91
Baekwife <krystal920917@gmail.com>
2025-04-04 23:28:18 +0800

    遊戲規則

---
+++ Assets/Scripts/RuleDescription.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PageNavigator : MonoBehaviour
+{
+    public string previousScene;
+    public string nextScene;
+
+    public void GoToPreviousScene()
+    {
+        if (!string.IsNullOrEmpty(previousScene))
+        {
+            SceneManager.LoadScene(previousScene);
+        }
+    }
+
+    public void GoToNextScene()
+    {
+        if (!string.IsNullOrEmpty(nextScene))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
+
