d712aa769ebae4eb8bdd78dd3372841989bfd334
kaocynthia99178 <kaocynthia99178@gmail.com>
2025-04-05 17:23:46 +0800

    Update change_card.cs

--- Assets/Scripts/change_card.cs
+++ Assets/Scripts/change_card.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,23 +8,20 @@ public class change_card : MonoBehaviour
     public Texture2D[] cards;
     public RawImage rawImages;
     private bool isEnlarged = false;
-    public int count = 0;
-     public float scale = 1/2;
+    public float scale = 1/2;
     public void chouca()
     {
         rawImages = rawImages.GetComponent<RawImage>();
         int result = Random.Range(0, 10);
         rawImages.texture = cards[result];
-
     }
    
     public void big()
     {
-        if(count == 0)
+        if(isEnlarged == false)
         {
             transform.localScale += new Vector3(scale, scale, 0f);
             isEnlarged = true;
-            count +=1;
         }
         else
         {
@@ -34,7 +32,6 @@ public class change_card : MonoBehaviour
     {
         transform.localScale -= new Vector3(scale, scale, 0f);
         isEnlarged = false;
-        count -= 1;
     }
     private void Update()
     {
