c4bb4542a930aede691c9e1a059fc5e8672be195
kaocynthia99178 <kaocynthia99178@gmail.com>
2025-05-23 02:54:54 +0800

    擲骰動畫改好但提示還沒

--- Assets/Scripts/ShowImageOnClick.cs
+++ Assets/Scripts/ShowImageOnClick.cs
@@ -5,26 +5,50 @@ public class ShowOrHideImageOnClick : MonoBehaviour
 {
     public Button toggleButton;   // 拖曳按鈕進來
     public GameObject imageObj;   // 拖曳要切換的Image進來
-
+    //public SpriteRenderer spriteObj;    // SpriteRenderer（非 UI Image）
     void Start()
+     {
+         if (toggleButton != null)
+         {
+             toggleButton.onClick.AddListener(ToggleImage);
+         }
+
+         if (imageObj != null)
+         {
+             imageObj.SetActive(false); // 一開始圖片隱藏
+         }
+     }   
+    void ToggleImage()
+     {
+         if (imageObj != null)
+         {
+
+             imageObj.SetActive(!imageObj.activeSelf);// 切換目前狀態
+             imageObj.transform.SetAsLastSibling();
+         }
+     }
+    
+    /*void Start()
     {
         if (toggleButton != null)
         {
             toggleButton.onClick.AddListener(ToggleImage);
         }
 
-        if (imageObj != null)
+        if (spriteObj != null)
         {
-            imageObj.SetActive(false); // 一開始圖片隱藏
+            spriteObj.enabled = false; // 一開始圖片隱藏
         }
-    }
 
+
+    }
     void ToggleImage()
     {
-        if (imageObj != null)
+        if (spriteObj != null)
         {
-            imageObj.SetActive(!imageObj.activeSelf);
-            imageObj.transform.SetAsLastSibling();// 切換目前狀態
+
+            spriteObj.enabled = !spriteObj.enabled; // 切換目前狀態
+            spriteObj.transform.SetAsLastSibling();
         }
-    }
+    }*/
 }
